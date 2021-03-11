dcceab0a9509d025a162409a575fbab44f2db977
SonamGadkari <S533490@nwmissouri.edu>
2021-03-10 19:40:26 -0500

    created recipe detail, recipe list, shopping-edit, shopping-list components

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+  /*public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number){
+    this.name= name;
+    this.amount= amount;
+  }*/
+// this below statement will crate the variables and intialize the values to the variables.
+  constructor(public name: string, public amount : number){}
+
+}
